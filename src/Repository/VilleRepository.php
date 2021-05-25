75541bd9b6684062f5f3a879b3a05fe7c3409d0e
Aucante <sebastien.aucante@gmail.com>
2021-05-25 11:49:51 +0200

    finalisation page gestion ville (sauf fonction rechercher inactive)

--- src/Repository/VilleRepository.php
+++ src/Repository/VilleRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Data\SearchData;
 use App\Entity\Ville;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -17,6 +18,13 @@ class VilleRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Ville::class);
+    }
+
+    public function findSearch(SearchData $search): array
+    {
+        $query = $this
+        ->createQueryBuilder('p');
+        return $query->getQuery()->getResult();
     }
 
     // /**
